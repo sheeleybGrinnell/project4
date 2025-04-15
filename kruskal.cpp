bc55e2444c7ac494983ebcf99d37d3a995bdd63f
root <root@Skeebo>
2025-04-15 08:21:50 -0500

    documentation for kruskal

--- kruskal.cpp
+++ kruskal.cpp
@@ -12,8 +12,15 @@
 
 using namespace std;
 
-/*
+/**
  * find
+ *      finds the parent vertex of any given vertex label
+ *  @param v
+ *      the label of the vertex we want to find the parent vertex of
+ *  @param pi
+ *      the vector which contains the information for the parent of each vertex
+ *  @return
+ *      the parent vertex of vertext labeled "v"
  */
 int find(int v, vector<int> &pi) {
     if (v == pi[v]) {
@@ -23,8 +30,17 @@ int find(int v, vector<int> &pi) {
     return pi[v];
 }
 
-/*
+/**
  * union_by_rank
+ *      unions two disjoint sets together based on any vertex within those disjoint sets
+ *  @param u
+ *      the first disjoint vertex we want to union
+ *  @param v
+ *      the second disjoint vertex we want to union
+ *  @param rank
+ *      the vector of vertices which contain rank based on label
+ *  @param pi
+ *      the vector of verticies which contain parent vertex based on label
  */
 void union_by_rank(int u, int v, vector<int> &rank, vector<int> &pi) {
     int vParent = find(pi[v], pi);
@@ -39,8 +55,17 @@ void union_by_rank(int u, int v, vector<int> &rank, vector<int> &pi) {
     return;
 }
 
-/*
+/**
  * kruskal
+ *      creates an MST using kruskal's algorithm
+ *      finds the smallest edge possible to add to an MST without creating cycles
+ *      we avoid creating cycles through use of disjoint sets (see find and union_by_rankmake)
+ *  @param adjList
+ *      an adjacency list which helps represent our "forest" or collection of vertices
+ *  @param edgeList
+ *      a sorted list of edges
+ *  @return
+ *      a minimum-spanning-tree containing the shortest path to each vertex
  */
 vector<Edge> kruskal(vector<Vertex> &adjList, vector<Edge> &edgeList) {
     // Initialize the empty MST.
@@ -54,23 +79,27 @@ vector<Edge> kruskal(vector<Vertex> &adjList, vector<Edge> &edgeList) {
     for (int i=0; i<pi.size(); i++) {
         pi[i] = i;
     }
-    // loop through each edge in our sorted edgelist
+    // loop through each edge in our sorted edgelist so we can add by priority
     for (Edge edge : edgeList) {
+        // look at the two vertices of our potential edge
         Vertex u = edge.first;
         Vertex v = edge.second;
+        // if both vertices are in the same set then it creates a cycle, so we should move on without this cycle
         if (find(u.label, pi) != find(v.label, pi)) {
+            // union the two disjoint sets each vertex populates
             union_by_rank(u.label, v.label, rank, pi);
             mst.push_back(edge);
+            // add both vertices to each others' neighbors list for the MST
             u.mstNeighbors.push_back(v.label);
-            for(int neighbor : u.mstNeighbors) {
-                cout << neighbor << '\n';
-            }
             v.mstNeighbors.push_back(u.label);
-            for (int neighbor : v.mstNeighbors) {
-                cout << neighbor << '\n';
-            }
+            // debug to find missing MST neighbors
+            cout << "u info\n";
+            u.printVertex();
+            cout << "v info\n";
+            v.printVertex();
 
-        }
+
+        } //if
     } //for
 
     return mst;
